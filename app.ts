ee501a9473318bb2e26689db10ac51dfd0805cdd
Federico Corradi <corradi.federico.91@gmail.com>
2021-03-06 18:11:33 +0000

    Type inference

--- app.ts
+++ app.ts
@@ -7,7 +7,9 @@ function add( n1: number, n2: number, showResult: boolean, phrase: string ) {
     }  
 }
 
-const number1 = 5;
+// telling typescrpit which type of value will be stored inside numeber1
+let number1: number;
+number1 = 5;
 const number2 = 2.8;
 const printResult = true;
 const resultPhrase = 'Result is '
