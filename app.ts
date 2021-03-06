7fb4e77a8c2f9f9be37e3d98fcb4ad6cb3cd672d
Federico Corradi <corradi.federico.91@gmail.com>
2021-03-06 19:30:05 +0000

    Literal Type

--- app.ts
+++ app.ts
@@ -1,7 +1,11 @@
-function combine( input1: number | string, input2: number | string ) { 
+function combine( 
+    input1: number | string, 
+    input2: number | string, 
+    resultConversion: 'as-numbers' | 'as-text' 
+    ) { 
     let result;
-    if ( typeof input1 === 'number' && typeof input2 === 'number' ) {
-        result = input1 + input2;
+    if ( typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-numbers' ) {
+        result = +input1 + +input2;
     } else {
         result = input1.toString() + input2.toString();
     }
@@ -9,8 +13,12 @@ function combine( input1: number | string, input2: number | string ) {
     return result;
 }
 
-const combinedAges = combine( 30, 26 );
+const combinedAges = combine( 30, 26, 'as-numbers' );
 console.log( combinedAges );
 
-const combinedNames = combine( 'Max', 'Anna' );
+const combinedStringAges = combine( '30', '26', 'as-numbers' )
+console.log( combinedStringAges );
+
+
+const combinedNames = combine( 'Max', 'Anna', 'as-text' );
 console.log( combinedNames );
