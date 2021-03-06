07b7972e91313f2772ef138ec822e773b9b1fcfb
Federico Corradi <corradi.federico.91@gmail.com>
2021-03-06 20:14:26 +0000

    Type Unknown

--- app.ts
+++ app.ts
@@ -1,34 +1,13 @@
-function add( n1: number, n2: number ) { 
-    return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+//All of the one below are valid
+userInput = 5;
+userInput = 'Max'
+
+// This shows that because userName is of tyoe string, we can't set to userInput as that is of valid unknown
+// the if statement is a workaorund to prevent typescrpit from showing an error
+if ( typeof userInput === `string` ) {
+    userName = userInput;
 }
 
-// The return type is a void type
-function printResult1( num: number ): void {
-    console.log( 'Result: ' + num );
-    // the function doesn't esplcitly return anything, which means that typescpit would consider the outcome void
-}
-
-function printResult2( num: number ):undefined {
-    console.log( 'Result: ' + num );
-    return;
-    // the function returns nothing but it still contains return, which would be euqual to undefined
-}
-
-// Esplicitely declaring the form of the callback functions
-function addAndHandle( n1: number, n2: number, cb: ( num: number ) => void ) {
-    const result = n1 + n2;
-    cb( result );
-}
-
-addAndHandle( 10, 20, (result) => {
-    console.log( result );
-})
-
-printResult1( add( 5, 12 ) )
-// Asserting that combinedValue shoudl hold a function, with two parameters number, returning a number
-let combinedValue: ( a: number, b: number) => number;
-combinedValue = add;
-// combinedValue = printResult1 -----> combinedValue must hold a function with the parameters number, returning a number *ERROR*
-// combinedValue = 5; -----> combinedValue must hold a function *ERROR*
-
-console.log( combinedValue( 8, 8 ) );
---
+++ functions.ts
@@ -0,0 +1,34 @@
+function add( n1: number, n2: number ) { 
+    return n1 + n2;
+}
+
+// The return type is a void type
+function printResult1( num: number ): void {
+    console.log( 'Result: ' + num );
+    // the function doesn't esplcitly return anything, which means that typescpit would consider the outcome void
+}
+
+function printResult2( num: number ):undefined {
+    console.log( 'Result: ' + num );
+    return;
+    // the function returns nothing but it still contains return, which would be euqual to undefined
+}
+
+// Esplicitely declaring the form of the callback functions
+function addAndHandle( n1: number, n2: number, cb: ( num: number ) => void ) {
+    const result = n1 + n2;
+    cb( result );
+}
+
+addAndHandle( 10, 20, (result) => {
+    console.log( result );
+})
+
+printResult1( add( 5, 12 ) )
+// Asserting that combinedValue shoudl hold a function, with two parameters number, returning a number
+let combinedValue: ( a: number, b: number) => number;
+combinedValue = add;
+// combinedValue = printResult1 -----> combinedValue must hold a function with the parameters number, returning a number *ERROR*
+// combinedValue = 5; -----> combinedValue must hold a function *ERROR*
+
+console.log( combinedValue( 8, 8 ) );
