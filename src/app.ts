707ebaddfde055dcfbd8f3dd5eb3567104b72c2e
Federico Corradi <corradi.federico.91@gmail.com>
2021-03-07 18:22:27 +0000

    Explaining classes

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,10 @@
-let userInput: unknown;
-let userName: string;
+class Department {
 
-//All of the one below are valid
-userInput = 5;
-userInput = 'Max'
+    name:string
 
-// This shows that because userName is of tyoe string, we can't set to userInput as that is of valid unknown
-// the if statement is a workaorund to prevent typescrpit from showing an error
-if ( typeof userInput === `string` ) {
-    userName = userInput;
+    constructor( n: string ) {
+        this.name = n;
+    }
 }
 
-// as this function throws an error, then it never return anything, that's why the type never
-function generateError( message:string, code: number ): never {
-    throw { message: message, errorCode: code }
-}
-
-generateError( 'An error occured', 500 )
-
+const accounting = new Department( 'Accounting' )
