d207dc09a3103849ecbfbed20f9552f60c54580b
Federico Corradi <corradi.federico.91@gmail.com>
2021-03-08 19:26:56 +0000

    More Type Guards

--- dist/app.js
+++ dist/app.js
@@ -4,3 +4,38 @@ const e1 = {
     priveleges: ['create-server'],
     startDate: new Date()
 };
+function add(n1, b) {
+}
+function printEmployeeInformation(emp) {
+    console.log('Name: ' + emp.name);
+    if ('priveleges' in emp) {
+        console.log('Privilage: ' + emp.priveleges);
+    }
+    if ('startDate' in emp) {
+        console.log('Start Date: ' + emp.startDate);
+    }
+}
+printEmployeeInformation(e1);
+class Car {
+    drive() {
+        console.log('Driving...');
+    }
+}
+class Truck {
+    drive() {
+        console.log('Driving truck...');
+    }
+    loadCargo(amount) {
+        console.log('Loading cargo:...' + amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
--- src/app.ts
+++ src/app.ts
@@ -20,3 +20,54 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+function add(n1:Combinable, b: Combinable) {
+   
+}
+
+type UknownEmployee = Employee | Admin;
+
+function  printEmployeeInformation(emp:UknownEmployee) {
+    console.log( 'Name: ' + emp.name);
+    if ( 'priveleges' in emp ) {
+        console.log( 'Privilage: ' + emp.priveleges );
+    }
+    if ( 'startDate' in emp ) {
+        console.log( 'Start Date: ' + emp.startDate );
+    }
+}
+
+printEmployeeInformation( e1 )
+
+class Car {
+    drive() {
+        console.log( 'Driving...' );
+        
+    }
+}
+
+class Truck {
+    drive() {
+        console.log( 'Driving truck...' );
+        
+    }
+
+    loadCargo( amount: number ) {
+        console.log( 'Loading cargo:...' + amount );
+        
+    }
+}
+
+type Vehicle = Car | Truck;
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+    vehicle.drive()
+    if( vehicle instanceof Truck ) {
+        vehicle.loadCargo(1000)
+    }
+}
+
+useVehicle( v1 )
+useVehicle( v2 )
