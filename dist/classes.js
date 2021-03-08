38b9d84ca0cfed3d2896685fa47e41e1dc024e2e
Federico Corradi <corradi.federico.91@gmail.com>
2021-03-08 18:23:45 +0000

    Start interfaces

---
+++ dist/classes.js
@@ -0,0 +1,72 @@
+"use strict";
+class Department {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.employees = [];
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+    static createEmployee(name) {
+        return { name: name };
+    }
+}
+Department.fiscalYear = 2020;
+class ITDepartment extends Department {
+    constructor(id, admins, reports = []) {
+        super(id, 'IT');
+        this.admins = admins;
+        this.reports = reports;
+        this.lastReport = reports[0];
+    }
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error("No report found");
+    }
+    set mostRecentReport(value) {
+        if (!value) {
+            throw new Error("Please pass in a value");
+        }
+        this.addReports(value);
+    }
+    static getInstance() {
+        if (ITDepartment.instance) {
+            return this.instance;
+        }
+        this.instance = new ITDepartment('LS', []);
+        return this.instance;
+    }
+    addEmployee(name) {
+        if (name === 'Max') {
+            return;
+        }
+        this.employees.push(name);
+    }
+    addReports(text) {
+        this.reports.push(text);
+        this.lastReport = text;
+    }
+    printReports() {
+        console.log(this.reports);
+    }
+    describe() {
+        console.log('IT Department - ID ' + this.id);
+    }
+}
+const IT = ITDepartment.getInstance();
+IT.addEmployee('Max');
+IT.addEmployee('Manu');
+IT.describe();
+IT.printEmployeeInformation();
+console.log(IT);
+IT.mostRecentReport = 'Year and report';
+console.log(IT.mostRecentReport);
+const employee1 = Department.createEmployee('Max');
+console.log(employee1, Department.fiscalYear);
--- src/app.ts
+++ src/app.ts
@@ -1,98 +1,18 @@
-abstract class Department {
-    protected employees: string[] = [] 
-    static fiscalYear = 2020;
-
-    constructor( protected readonly id: string, public name: string ) {
-    }
-
-    abstract describe( this: Department ): void;
-
-    addEmployee( employee: string ) {
-        this.employees.push( employee )
-    }
-
-    printEmployeeInformation() {
-        console.log( this.employees.length );
-        console.log( this.employees );
-    }
-
-    static createEmployee( name:string ) {
-        return { name: name }
-    }
+interface Person {
+    name: string;
+    age: number;
+    greet( phrase: string ): void
 }
 
-class ITDepartment extends Department {
+let user1: Person;
 
-    private lastReport: string;
-    private static instance: ITDepartment
-
-
-    get mostRecentReport() {
-        if ( this.lastReport ) {
-            return this.lastReport;
-        }
-        throw new Error( "No report found" )
-    }
-
-    set mostRecentReport( value: string ) {
-        if( !value ) {
-            throw new Error( "Please pass in a value" )
-        }
-        this.addReports( value )
-    }
-
-    private constructor( id: string, public admins: string[], private reports: string[] = [] ) {
-        super(id, 'IT');
-        this.lastReport = reports[0]
-    }
-
-    static getInstance() {
-        if ( ITDepartment.instance ) {
-            return this.instance;
-        }
-        this.instance = new ITDepartment('LS', [])
-        return this.instance
-    }
-
-    addEmployee( name: string ) {
-        if ( name === 'Max' ) {
-            return
-        }
-        this.employees.push( name )
-    }
-
-    addReports( text: string ) {
-        this.reports.push( text )
-        this.lastReport = text;
-    }
-
-    printReports() {
-        console.log( this.reports );
-        
-    }
-
-    describe(  ) {
-        console.log( 'IT Department - ID ' + this.id );
+user1 = {
+    name: 'Max',
+    age: 30,
+    greet(phrase: string) {
+        console.log( phrase + '' + this.name );
         
     }
 }
 
-const IT = ITDepartment.getInstance();
-
-IT.addEmployee( 'Max' );
-IT.addEmployee( 'Manu' );
-
-IT.describe();
-IT.printEmployeeInformation();
-
-console.log( IT );
-
-IT.mostRecentReport = 'Year and report'
-console.log( IT.mostRecentReport );
-
-const employee1 = Department.createEmployee('Max')
-console.log( employee1, Department.fiscalYear );
-
-
-// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
-// accountingCopy.describe();
+user1.greet( 'Hi there + I' )
