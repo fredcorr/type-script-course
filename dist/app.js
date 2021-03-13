fe861504084aa99c2f43c0ba913d13874373d641
Federico Corradi <Federico Corradi>
2021-03-13 17:37:17 +0000

    The keyof constrain

--- dist/app.js
+++ dist/app.js
@@ -4,3 +4,14 @@ function merge(objsA, objsB) {
 }
 const mergeObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
 console.log(mergeObj);
+function countAndDescribe(element) {
+    let descriptionText = 'Got no value';
+    if (element.length === 1) {
+        descriptionText = 'Got 1 value';
+    }
+    else if (element.length > 1) {
+        descriptionText = 'Got ' + element.length + ' value';
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe('Hi there'));
--- src/app.ts
+++ src/app.ts
@@ -17,4 +17,21 @@ function merge<T extends object, U extends object>(objsA: T, objsB: U) {
 const mergeObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30} );
 console.log( mergeObj );
 
+interface Lengthy {
+    length: number;
+}
+
+function countAndDescribe<T extends Lengthy>( element: T ): [ T, string ] {
+    let descriptionText = 'Got no value';
+
+    if( element.length === 1 ) {
+        descriptionText = 'Got 1 value'
+    } else if ( element.length > 1 ) {
+        descriptionText = 'Got ' + element.length + ' value'
+    }
+
+    return [ element, descriptionText ]
+}
+
+console.log( countAndDescribe( 'Hi there' ) );
 
